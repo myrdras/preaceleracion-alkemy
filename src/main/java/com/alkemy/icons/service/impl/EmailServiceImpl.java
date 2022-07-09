efbca56a19c59f2078cdf4b20730a133ef031749
unknown <mgespindola93@gmail.com>
2022-07-09 00:21:52 -0300

    Finish SendGrid

--- src/main/java/com/alkemy/icons/service/impl/EmailServiceImpl.java
+++ src/main/java/com/alkemy/icons/service/impl/EmailServiceImpl.java
@@ -1,10 +1,14 @@
 package com.alkemy.icons.service.impl;
 
 import com.alkemy.icons.service.EmailService;
+import com.sendgrid.Method;
+import com.sendgrid.Request;
+import com.sendgrid.Response;
 import com.sendgrid.SendGrid;
 import com.sendgrid.helpers.mail.Mail;
 import com.sendgrid.helpers.mail.objects.Content;
 import com.sendgrid.helpers.mail.objects.Email;
+import java.io.IOException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.env.Environment;
@@ -35,7 +39,19 @@ public class EmailServiceImpl implements EmailService {
         
         Mail mail = new Mail(fromEmail, subject, toEmail, content);
         SendGrid sg = new SendGrid(apiKey);
-        
+        Request request = new Request();
+        try {
+            request.setMethod(Method.POST);
+            request.setEndpoint("mail/send");
+            request.setBody(mail.build());
+            Response response = sg.api(request);
+            
+            System.out.println(response.getStatusCode());
+            System.out.println(response.getBody());
+            System.out.println(response.getHeaders());
+        } catch (IOException e) {
+            System.out.println("Error trying to send the email");
+        }
     }
 
 }
